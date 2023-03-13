a66a8059b14e5f2b522899dac329663095b7775f
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-03-13 18:40:37 -0300

    First commit

---
+++ react-project1/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export function Home() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <input type="text" placeholder="Digite o nome..."/>
+      <button type="button" >Adicionar</button>
+    </div>
+  )
+}
+
+
