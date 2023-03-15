01d6ceb77a1cf6a561bf6eb8aed33c5bb4c1c610
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-03-15 18:33:08 -0300

    Fundamentos React

--- react-project1/reactapp/src/pages/Home/index.jsx
+++ react-project1/reactapp/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 
 import { Card } from '../../components/Card'
@@ -6,6 +6,8 @@ import { Card } from '../../components/Card'
 export function Home() {
   const [studentName, setStudentName] = useState('')
   const [students, setStudents] = useState([])
+  const [user, setUser] = useState({name: '', avatar: ''})
+  //useState tem como primeiro valor o nome do estado e como segundo a função que muda esse estado
 
   function handleAddStudent(){
     const newStudent = {
@@ -21,10 +23,30 @@ export function Home() {
   }
 
  
+  useEffect(() => {
+    //corpo do useEffect(é executado assim que a interface é renderizada)
+    async function renderUser(){
+      const response = await fetch('https://api.github.com/users/ythiago03')
+      const data = await response.json()
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url
+      })
+    }
+
+    renderUser()
+  }, [])//o array recebe quais os estados que o useEffect depende
 
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
+      
       <input 
         type="text" 
         placeholder="Digite o nome..."
@@ -35,7 +57,12 @@ export function Home() {
       </button>
 
       {
-        students.map(student => <Card name={student.name} time={student.time}/>)
+        students.map(student => (
+          <Card 
+            key={student.time}
+            name={student.name} 
+            time={student.time}/>
+          ))
       }
     </div>
   )
